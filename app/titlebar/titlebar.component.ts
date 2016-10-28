c35cac81b4388e914c8f8fbaaf4a1edd7d457bfa
Sylvain RAGOT <sylvain.ragot@predicsis.com>
2016-10-28 20:29:41 +0200

    Design titlebar for the translations view

--- app/titlebar/titlebar.component.ts
+++ app/titlebar/titlebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: 'titlebar.component.html',
   styleUrls: [ 'titlebar.component.css' ]
 })
-export class TitlebarComponent {}
+export class TitlebarComponent implements OnInit {
+  isProjectsList: boolean;
+  isTranlationsList: boolean;
+
+  @Input() owner: string;
+  @Input() repo: string;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.isProjectsList = false;
+    this.isTranlationsList = false;
+  }
+
+  ngOnInit(): void {
+    let routeName = this.route.routeConfig.path;
+    // see app.routing.ts to the list of possible values
+
+    this.isProjectsList = routeName === 'projects';
+    this.isTranlationsList = routeName.indexOf('translations') === 0;
+  }
+
+  onClickProjects(): void {
+    this.router.navigate(['/projects']);
+  }
+
+  onOpenBranchSwitcher(): void {}
+  onFold(): void {}
+  onDiff(): void {}
+  onUnfold(): void {}
+  onExport(): void {}
+  onCommit(): void {}
+}
