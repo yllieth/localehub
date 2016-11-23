d6b618e8aba36c5d0f76d94fc9f3f9d7ce11e3c5
Sylvain RAGOT <sylvnimes@hotmail.com>
2016-11-23 12:43:49 +0100

    [Login] Github authentication
    - redirect the user on a github login page to get an oauth code
    - hit an aws lambda which will handle all oauth requests, store github access token and return an internal token

--- app/+services/index.ts
+++ app/+services/index.ts
@@ -1,4 +1,5 @@
 export * from './error.service';
 export * from './events.service';
+export * from './github-auth.service';
 export * from './projects.service';
 export * from './translations.service';
--- app/shared/login/login.component.ts
+++ app/shared/login/login.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Http } from '@angular/http';
+import { GithubAuthService } from '../../+services';
 
 @Component({
   moduleId: module.id,
   selector: 'lh-login',
   templateUrl: 'login.component.html',
-  styleUrls: [ 'login.component.css' ]
+  styleUrls: [ 'login.component.css' ],
+  providers: [ GithubAuthService ]
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  constructor(
+    private $route: ActivatedRoute,
+    private $router: Router,
+    private $http: Http,
+    private auth: GithubAuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.$route.queryParams.forEach((query: Params) => {
+      if (this.auth.isOauthStep1(query) === true) {
+        // hide state & code from the address bar
+        this.$router.navigate(['/login']);
+
+        // oauthStep1
+        this.auth
+          .getAccessToken(query['code'])
+          .map(res => res.json())
+          .subscribe(token => console.log(token));
+      } else {
+        // do nothing
+        // go there when the user manually navigate to /login or is redirected by the AuthGuard
+      }
+    })
+  }
+
+  startOauthDance(): string {
+    return this.auth.createAuthorizationUrl();
+  }
+}
