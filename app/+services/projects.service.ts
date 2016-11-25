8b6f96a1ce584e34a2ba3601deb334c6bd24fbb2
Sylvain RAGOT <sylvnimes@hotmail.com>
2016-11-25 09:34:52 +0100

    [Core] Use mock server to retrieve a project

--- app/+services/projects.service.ts
+++ app/+services/projects.service.ts
@@ -7,7 +7,8 @@ import { ApiService, AuthenticationService } from './';
 
 @Injectable()
 export class ProjectsService {
-  private projectsUrl = ApiService.baseUrl + '/projects';
+  private all = () => `${ApiService.baseUrl}/projects`;
+  private one = (owner: string, repo: string) => `${ApiService.baseUrl}/projects/${owner}/${repo}`;
 
   constructor(private $http: Http) {}
 
@@ -16,24 +17,20 @@ export class ProjectsService {
     let options = new RequestOptions({ headers: headers });
 
     return this.$http
-      .get(this.projectsUrl, options)
+      .get(this.all(), options)
       .toPromise()
       .then((response: Response) => response.json() as Group[])
       .catch(error => Promise.reject(error));
   }
 
   getProject(owner: string, repo: string): Promise<Project> {
-    return this.getProjectList()
-      .then((projectList: Group[]) => {
-        let group = projectList.find(function(project: Group) {
-          return project.user.pseudo === owner;
-        });
+    let headers = new Headers({ 'Authorization': AuthenticationService.getToken() });
+    let options = new RequestOptions({ headers: headers });
 
-        let projects = group.projects;
-
-        return projects.find(function(project: Project) {
-          return project.name === repo;
-        });
-      })
+    return this.$http
+      .get(this.one(owner, repo), options)
+      .toPromise()
+      .then((response: Response) => response.json() as Project)
+      .catch(error => Promise.reject(error));
   }
 }
