7f3afa473977cb60c229668d9d7924087225541d
Sylvain RAGOT <sylvnimes@hotmail.com>
2016-11-29 10:54:23 +0100

    [Backend] Save lambdas
    - api-authorizer
    - POST /login
    - GET /organizations
    
    Notes:
    login lambda is defined with the following environment variables setted: client_secret, client_id, redirect_url

---
+++ lamdas/api-authorizer.py
@@ -0,0 +1,31 @@
+import boto3
+
+client_dynamo = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    token = event['authorizationToken']
+    
+    
+    auth_info = client_dynamo.get_item(TableName='tokens', Key={'token': { 'S': token } }).get('Item')
+    
+    if (auth_info):
+        print 'sfsdff'
+        print auth_info
+        return generatePolicy('user', event['methodArn'], { 'githob': auth_info['github_token']['S'] })
+    else:
+        print 'no auth'
+        raise Exception('Unauthorized')
+    
+def generatePolicy(principalId, resource, context):
+    return {
+        'principalId': principalId,
+        'policyDocument': {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Action': 'execute-api:Invoke',
+                'Effect': 'Allow',
+                'Resource': resource
+            }]
+        },
+        'context': context
+    }
---
+++ lamdas/organizations.py
@@ -0,0 +1,33 @@
+import json
+import httplib
+
+def organizations_request(access_token):
+    method = "GET"
+    endpoint = "api.github.com"
+    url = "/user/orgs"
+    headers = {
+        "Authorization": "token " + access_token,   # https://developer.github.com/v3/#oauth2-token-sent-in-a-header
+        "Content-Type": "application/json",
+        "User-Agent": "Localehub"                   # https://developer.github.com/v3/#user-agent-required
+    }
+
+    conn = httplib.HTTPSConnection(endpoint)
+    conn.request(method, url, None, headers)
+    return conn.getresponse()
+
+def lambda_handler(event, context):
+    github_token = event['requestContext']['authorizer']['githob']
+    response = organizations_request(github_token)
+    status = response.status
+    data = json.loads(json.dumps(response.read()))
+    #print(response.status, response.reason) # 200 OK
+    #print(response.read())
+
+    return {
+        "statusCode": status,
+        "body": data,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        }
+    }
