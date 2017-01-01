13787c8c4efce83f116142a9b455fa71e3b2e1d9
Sylvain RAGOT <sylvnimes@hotmail.com>
2017-01-01 22:15:16 +0100

    [Backend] Save authorizer lambda

--- lambdas/api-authorizer.py
+++ lambdas/api-authorizer.py
@@ -12,7 +12,9 @@ def lambda_handler(event, context):
     else:
         print 'no auth'
         raise Exception('Unauthorized')
-    
+
+# As the policy is cached, it cannot be generated for a specific resource
+# See http://stackoverflow.com/a/41417324/1230946
 def generatePolicy(principalId, resource, context):
     return {
         'principalId': principalId,
@@ -21,7 +23,8 @@ def generatePolicy(principalId, resource, context):
             'Statement': [{
                 'Action': 'execute-api:Invoke',
                 'Effect': 'Allow',
-                'Resource': resource
+                'Resource': 'arn:aws:execute-api:eu-central-1:673077269136:hrjyk83zp5/prod/*'   # Complete arn looks like .../prod/GET/organizations
+                #'Resource': resource
             }]
         },
         'context': context
