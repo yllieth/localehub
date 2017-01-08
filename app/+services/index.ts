d02a316540693abad7d75a60003b388854f24831
Sylvain RAGOT <sylvnimes@hotmail.com>
2017-01-08 22:33:02 +0100

    [Translations] Add a FlagService
    The service associates a country code to a bunch of details as language name, css class to display the flag (with the help of flag-icon package)

--- app/+services/index.ts
+++ app/+services/index.ts
@@ -5,3 +5,4 @@ export * from './events.service';
 export * from './authentication.service';
 export * from './projects.service';
 export * from './translations.service';
+export * from './flag.service';
