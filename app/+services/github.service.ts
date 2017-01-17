01e984b02e8a02c2127555e8a569091382d8241d
yllieth <sylvnimes@hotmail.com>
2017-01-17 18:26:59 -0500

    [Refactor] move getOrganizations() in the GithubService

--- app/+services/github.service.ts
+++ app/+services/github.service.ts
@@ -2,18 +2,43 @@ import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
+import { GithubRepository, User } from '../+models';
 import { ApiService } from './';
-import { GithubRepository } from "../+models";
 
 @Injectable()
 export class GithubService {
   constructor(private api: ApiService) {}
 
+  /**
+   * Get repositories of the user in parameter.
+   * Github base request : GET api.github.com/users/{username}/repos
+   *   | using lambda: gh-get-users-repos
+   *   | using lambda: repositories
+   *
+   * @param {string} username - REQUIRED
+   * @returns {Promise<GithubRepository[]>}
+   */
   getRepositories(username: string): Promise<GithubRepository[]> {
     return this.api
       .get(`${ApiService.endpoint.prod}/repositories/${username}`)
       .toPromise()
       .then((response: Response) => response.json() as GithubRepository[])
+      .catch(error => Promise.reject(error));
+  }
+
+  /**
+   * Get organizations of the connected user.
+   * Github base request : GET api.github.com/user/orgs
+   *   | using lambda: gh-get-user-orgs
+   *   | using lambda: organizations
+   *
+   * @returns {Promise<User[]>}
+   */
+  getOrganizations(): Promise<User[]> {
+    return this.api
+      .get(`${ApiService.endpoint.prod}/organizations`)
+      .toPromise()
+      .then((response: Response) => response.json() as User[])
       .catch(error => Promise.reject(error));
   }
 }
