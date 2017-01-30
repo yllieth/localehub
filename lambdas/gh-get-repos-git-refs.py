b27bd6ccd632374b04a4d92e818e849d290b9fc2
yllieth <sylvnimes@hotmail.com>
2017-01-30 12:10:08 -0500

    [Authentication] Use the repo scope to be able to create new branches

---
+++ lambdas/gh-get-repos-git-refs.py
@@ -0,0 +1,49 @@
+import json
+import httplib
+import urllib
+
+# See https://developer.github.com/v3/git/refs/#get-a-reference
+# A complete github ref looks like:
+# {
+#   "ref": "refs/heads/featureA",
+#   "url": "https://api.github.com/repos/octocat/Hello-World/git/refs/heads/featureA",
+#   "object": {
+#     "type": "commit",
+#     "sha": "aa218f56b14c9653891f9e74264a383fa43fefbd",
+#     "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/aa218f56b14c9653891f9e74264a383fa43fefbd"
+#   }
+# }
+def branches_request(access_token, owner, repo, branch):
+    method = "GET"
+    endpoint = "api.github.com"
+    url = "/repos/" + owner + "/" + repo + "/git/refs/heads/" + branch
+    headers = {
+        "Authorization": "token " + access_token,   # https://developer.github.com/v3/#oauth2-token-sent-in-a-header
+        "Content-Type": "application/json",
+        "User-Agent": "Localehub"                   # https://developer.github.com/v3/#user-agent-required
+    }
+
+    print(method, endpoint + url, 'token ' + access_token)
+    conn = httplib.HTTPSConnection(endpoint)
+    conn.request(method, url, None, headers)
+    return conn.getresponse()
+
+def lambda_handler(event, context):
+    github_token = event['requestContext']['authorizer']['githob']
+    owner = event['pathParameters']['owner']
+    repo  = event['pathParameters']['repo']
+    ref   = event['pathParameters']['ref']
+    response = branches_request(github_token, owner, repo, ref)
+    status = response.status
+    data = json.loads(json.dumps(response.read()))
+    #print(response.status, response.reason) # 200 OK
+    #print(response.read())
+
+    return {
+        "statusCode": status,
+        "body": data,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        }
+    }
