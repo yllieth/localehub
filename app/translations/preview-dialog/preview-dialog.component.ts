87feb67456e403a2d893c033b4d28d9ecbffd195
yllieth <sylvnimes@hotmail.com>
2017-02-03 00:11:04 -0500

    [Translations] Show differences

--- app/translations/preview-dialog/preview-dialog.component.ts
+++ app/translations/preview-dialog/preview-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MdDialogRef } from '@angular/material';
-import { Project } from '../../+models';
+
+import { Language, LocaleUpdate, Project } from '../../+models';
+import { LanguageService } from '../../+services';
+
 
 @Component({
   moduleId: module.id,
@@ -9,13 +12,41 @@ import { Project } from '../../+models';
   styleUrls: [ 'preview-dialog.component.css' ]
 })
 export class TranslationsPreviewDialog implements OnInit {
-  project: Project; // from TitleBarComponent.openPreviewDialog : translationsPreviewDialog.componentInstance.project = project;
+  project: Project;       // from TitleBarComponent.openPreviewDialog : translationsPreviewDialog.componentInstance.project = project;
+  languages: Language[];  // list of unique languages contained in pendingChanges
+  totalChanges: number;
+  changes;                // {'en-US': <LocaleUpdate[]>, ...} of pendingChanges
+  files;                  // {'en-US': <I18nFileInfo>, ...} of pendingChanges
 
   constructor(
     public translationsPreviewDialog: MdDialogRef<TranslationsPreviewDialog>
   ) { }
 
   ngOnInit() {
+    this.languages = [];
+    this.changes = {};
+    this.files = {};
+    this.totalChanges = 0;
 
+    for (let change of this.project.pendingChanges) {
+      if (change.branch === this.project.lastActiveBranch) {
+        this.totalChanges++;
+
+        // build languages array
+        if (this.languages.filter(language => language.languageCode === change.languageCode).length <= 0) {
+          this.languages.push(LanguageService.find(change.languageCode));
+        }
+
+        // build changes object
+        if (this.changes.hasOwnProperty(change.languageCode) === true) {
+          this.changes[change.languageCode].push(change);
+        } else {
+          this.changes[change.languageCode] = [change];
+        }
+
+        // build files object
+        this.files[change.languageCode] = this.project.i18nFiles.filter(file => file.languageCode === change.languageCode)[0];
+      }
+    }
   }
 }
